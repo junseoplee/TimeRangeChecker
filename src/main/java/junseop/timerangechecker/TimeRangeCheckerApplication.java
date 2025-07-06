19658973ef501f3bd63922399df760c500df6d2f
junseoplee <junseoplee@soongsil.ac.kr>
2025-07-06 15:48:33 +0900

    Initial commit

---
+++ src/main/java/junseop/timerangechecker/TimeRangeCheckerApplication.java
@@ -0,0 +1,13 @@
+package junseop.timerangechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRangeCheckerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TimeRangeCheckerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/junseop/timerangechecker/TimeRangeCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package junseop.timerangechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeRangeCheckerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
